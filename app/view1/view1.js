8bffc1f0a2c2b665a2b622e3bae2b9fefd8163df
Michael Fleming <michael.fleming@accesso.com>
2016-05-17 17:55:39 -0400

    - Added in formatted less options - Will most likely need to modify a template file instead of a string - Needs to retain indenting, formatting, etc.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -16,42 +16,50 @@ angular.module('myApp.view1', ['ngRoute'])
         {
             'name': 'nav-bg',
             'placeholder': 'Nav Background',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@navBG'
         },
         {
             'name': 'nav-fg',
             'placeholder': 'Nav Foreground',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@navFG'
         },
         {
             'name': 'tagline',
             'placeholder': 'Tagline Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@tagline'
         },
         {
             'name': 'buy-bg',
             'placeholder': 'Buy BG Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@buyBG'
         },
         {
             'name': 'buy-fg',
             'placeholder': 'Buy FG Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@buyFG'
         },
         {
             'name': 'price-bg',
             'placeholder': 'Price BG Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@priceBG'
         },
         {
             'name': 'price-fg',
             'placeholder': 'Price FG Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@priceFG'
         },
         {
             'name': 'shadow',
             'placeholder': 'Shadow Color',
-            'additional': 'e.g. #ff0000'
+            'additional': 'e.g. #ff0000',
+            'replace': '@@shadow'
         }
     ]
 
@@ -76,6 +84,19 @@ angular.module('myApp.view1', ['ngRoute'])
             }
         })
     }, true);
+
+    $scope.generateLESS = function(fields) {
+        // TODO: split into file and load dynamically
+        $scope.generatedLESS = '.fix-navigation{background-color:@@navBG}.desktop-navigation>.top-bar-block{color:@@navFG;&:before{background-color:#fff}&.top-bar-promo:after{background-color:#fff}}.package-item-price{background-color:@@priceBG;.price-tag{color:@@priceFG}.buy-btn{background-color:@@buyBG color:@@buyFG &:before{border-color:@@shadow transparent transparent transparent!important}&:after{border-color:@@priceBG transparent transparent transparent!important}}}.top-bar-branding .branding .branding-title{color:@@navFG!important}.panel-heading{.package-heading{color:@@tagline!important}}';
+
+        fields.forEach(function(item){
+            var pattern = item.replace,
+                re = new RegExp(pattern, "g");
+            $scope.generatedLESS = $scope.generatedLESS.replace(re, item.modelValue);
+        })
+
+        $scope.showGeneratedLESS = true;
+    }
 
 
 }]);
